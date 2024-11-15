ac53804ea69e3fbb85b4fa804ca063d2da4a2f12
Niko Dittmar <nikodittmar@gmail.com>
2024-11-15 13:31:40 -0800

    Finished

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// COMSC 210 | Lab 32 | Niko Dittmar
 #include "Car.h"
 #include <iostream>
 #include <deque>
@@ -11,18 +12,41 @@ int main() {
 
     deque<Car> toll_booth;
 
+    cout << "Initial queue:" << endl;
+
     for (int i = 0; i < START_SIZE; i++) {
-        toll_booth.push_back(Car());
+        Car toAdd = Car();
+        toll_booth.push_back(toAdd);
+        cout << "   ";
+        toAdd.print();
     }
 
+    int time = 0;
     while (toll_booth.size() != 0) {
+        time += 1;
+        cout << "Time: " << time << " Operation: ";
         int num = rand() % 100 + 1;
         if (num <= 55) {
-
+            cout << "Car paid: ";
+            Car removed = toll_booth.front();
+            removed.print();
+            toll_booth.pop_front();
         } else {
-            
+            cout << "Joined lane: ";
+            Car toAdd = Car();
+            toAdd.print();
+            toll_booth.push_back(toAdd);
         }
+        cout << "Queue: " << endl;
+        if (toll_booth.size() == 0) {
+            cout << "   Empty" << endl;
+        } else {
+            for (Car car : toll_booth) {
+                cout << "   ";
+                car.print();
+            }
+        }
+        cout << endl;
     }
-    cout << "Hello World!";
     return 0;
 }
