f312606648ec11e7ecd615c8216d72de7faf6dc5
Niko Dittmar <nikodittmar@gmail.com>
2024-11-15 19:29:39 -0800

    Added chance constant

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 const int START_SIZE = 2;
+const int CHANCE_PAID = 55;
+const int CHANCE_JOIN = 45;
 
 int main() {
     srand(time(0));
@@ -26,12 +28,14 @@ int main() {
         time += 1;
         cout << "Time: " << time << " Operation: ";
         int num = rand() % 100 + 1;
-        if (num <= 55) {
+        if (num <= CHANCE_PAID) {
             cout << "Car paid: ";
             Car removed = toll_booth.front();
             removed.print();
             toll_booth.pop_front();
-        } else {
+        } 
+        num = rand() % 100 + 1;
+        if (num <= CHANCE_JOIN) {
             cout << "Joined lane: ";
             Car toAdd = Car();
             toAdd.print();
